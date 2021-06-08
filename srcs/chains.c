1ebdc8ad5d46ad9ee1a7644dfdc6d149d42e32d0
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-06-08 07:49:30 +0100

    lots of trouble on the sequences 😗 testing with 1 5 2 3 6 7

--- srcs/chains.c
+++ srcs/chains.c
@@ -6,7 +6,7 @@
 /*   By: apinto <apinto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 05:58:17 by apinto            #+#    #+#             */
-/*   Updated: 2021/06/07 11:32:36 by apinto           ###   ########.fr       */
+/*   Updated: 2021/06/08 07:48:09 by apinto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,8 @@ static	void merge_sequences(array *array, int *start, int *end)
 {
 	int i;
 	int j;
-	int ones;
+	int pot_ones;
+	int lar_ones;
 
 	if (start)
 		i = *start - 1;
@@ -67,15 +68,25 @@ static	void merge_sequences(array *array, int *start, int *end)
 		j = array->size;
 
 	printer(array, 0);
-	ones = count_ones(array->pot_seq_buf, 0, array->count);
-	printf("pot ones: %d\nlargest ones: %d\n", ones, array->largest_chain_size);
-	if (ones > array->largest_chain_size)
+	pot_ones = count_ones(array->pot_seq_buf, 0, j);
+	lar_ones = count_ones(array->largest_seq_buf, 0, j);
+	printf("pot ones: %d\nlargest ones: %d\n", pot_ones, lar_ones);
+	/* up until to the index *end*, there is a larger sequence in
+	 * the potential buffer */
+	if (pot_ones > lar_ones)
 	{
 		while (++i <= j)
 			array->largest_seq_buf[i] = array->pot_seq_buf[i];
-		array->largest_chain_size = ones;
+		if (j == array->count)
+			array->largest_chain_size = pot_ones;
+		if (start)
+			array->start_of_largest_chain = *start;
+		else
+			array->start_of_largest_chain = 0;
+
 	}
-	ft_bzero(array->pot_seq_buf, array->size);
+	if (!start && !end)
+		ft_bzero(array->pot_seq_buf, array->size);
 }
 
 /* checks for the longest chain of ordered numbers;
@@ -88,22 +99,36 @@ static	void merge_sequences(array *array, int *start, int *end)
 /* first call is check_chain_rec(array, 0, 0, 0) */
 static void	check_chain_rec(array *array, int start, int last, int pos)
 {
+	int advance;
+	int sum;
+
+	advance = 0;
+	/* this feels unnecessary ⤵️ */
 	array->pot_seq_buf[start] = 1;
 	/* if you get to the end of the array,
 	 * there were no conflicting chains and thus
 	 * the longest chain shall be saved */
-	if (start == array->count || pos == array->count)
+	if (start >= array->count || pos >= array->count)
 		merge_sequences(array, NULL, NULL);
 	/* a candidate for continuation of the sequence;
 	 * checks if the candidate was already in a larger sequence.
 	 * otherwise, keep looking forward */
-	else if (array->stack[pos] >= array->stack[last])
+	else if (array->stack[pos] > array->stack[last])
 	{
+		/* in this case, advance the index until
+		 * the first zero is found */
 		array->pot_seq_buf[pos] = 1;
-		if (array->largest_seq_buf[pos] == 1)
+		if (array->largest_seq_buf[pos + advance] == 1 && start == array->start_of_largest_chain)
+			return;
+		while ((pos + advance < array->count) && array->largest_seq_buf[pos + advance] == 1)
+		{
+			array->pot_seq_buf[pos + advance] = array->largest_seq_buf[pos + advance];
+			advance++;
+		}
+		sum = pos + advance;
+		if (advance > 0)
 			merge_sequences(array, &start, &pos);
-		else
-			check_chain_rec(array, start, pos, pos + 1);
+		check_chain_rec(array, start, pos + advance, pos + advance + 1);
 	}
 	else
 		check_chain_rec(array, start, last, pos + 1);
@@ -122,6 +147,7 @@ int	run_checks(array *array)
 		while (++j < array->count)
 		{
 			check_chain_rec(array, i, i, j);
+			printer(array, 0);
 			if (array->largest_chain_size > array->count - 1)
 				return array->largest_chain_size;
 		}
